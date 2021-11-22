b2da4ea55106a99c0ff974b18118a09374cf1acb
Jesse Malone <jmalone@vmware.com>
2021-11-22 10:46:52 -0500

    Don't need to install python-lsp, will install automatically

--- install.sh
+++ install.sh
@@ -9,8 +9,5 @@ cabal install haskell-language-server
 # js/typescript
 npm i -g typescript-language-server typescript
 
-# python
-pip3 install python-lsp-server
-
 cp ~/.emacs ~/.emacs.bak.$(date +%d%m%y-%H%m%S)
 cp .emacs ~/.emacs
